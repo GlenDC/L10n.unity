1bd589b995e39d3431967a346a28ca3bfec3c7b8
decauwsemaecker.glen@gmail.com <decauwsemaecker.glen@gmail.com>
2016-03-17 20:14:33 -0300

    adding first crappy variable-feature to translation will need to look into a decent way to serialize this (SerializbleObject perhaps?)

--- package/Assets/L20n/src/components/L20nSubmitLocale.cs
+++ package/Assets/L20n/src/components/L20nSubmitLocale.cs
@@ -3,16 +3,19 @@ using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
-[AddComponentMenu("L20n/SubmitLocale")]
-public sealed class L20nSubmitLocale : MonoBehaviour {
-	[SerializeField] private string m_LocaleIdentifier = null;
+namespace L20nUnity
+{
+	[AddComponentMenu("L20n/SubmitLocale")]
+	public sealed class L20nSubmitLocale : MonoBehaviour {
+		[SerializeField] private string m_LocaleIdentifier = null;
 
-	void Start () {
-		Debug.Assert(m_LocaleIdentifier != null && m_LocaleIdentifier != "",
-		             "<L20nSubmitLocale> requires a local identifier to be specified");
-	}
+		void Start () {
+			Debug.Assert(m_LocaleIdentifier != null && m_LocaleIdentifier != "",
+			             "<L20nSubmitLocale> requires a local identifier to be specified");
+		}
 
-	public void OnSubmit() {
-		L20n.SetLocale(m_LocaleIdentifier);
+		public void OnSubmit() {
+			L20n.SetLocale(m_LocaleIdentifier);
+		}
 	}
 }
--- package/Assets/L20n/src/components/L20nText.cs
+++ package/Assets/L20n/src/components/L20nText.cs
@@ -1,25 +1,74 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
+
+using System;
 using System.Collections.Generic;
 
-[AddComponentMenu("L20n/Text")]
-public sealed class L20nText : MonoBehaviour {
-	[SerializeField] private string m_Identifier;
-	private Text m_TextComponent;
+using UserVariable = L20nCore.UserVariable;
 
-	void Start () {
-		m_TextComponent = GetComponent<Text>();
-		Debug.Assert(
-			m_TextComponent != null,
-			"<L20nText> requires a <TextComponent> to be attached");
-		Debug.Assert(
-			m_Identifier != null && m_Identifier != "",
-			"<L20nText> requires a <string-identifier> to be attached");
+namespace L20nUnity
+{
+	[AddComponentMenu("L20n/Text")]
+	public sealed class L20nText : MonoBehaviour {
+		[SerializeField] private string m_Identifier;
+		[SerializeField] private List<L20nVariable> m_Variables = new List<L20nVariable>();
+
+		private Text m_TextComponent;
+		private L20nCore.UserVariables m_UserVariables;
+
+		void Start () {
+			m_TextComponent = GetComponent<Text>();
+			Debug.Assert(
+				m_TextComponent != null,
+				"<L20nText> requires a <TextComponent> to be attached");
+			Debug.Assert(
+				m_Identifier != null && m_Identifier != "",
+				"<L20nText> requires a <string-identifier> to be attached");
+
+			m_UserVariables = new L20nCore.UserVariables();
+			for(int i = 0; i < m_Variables.Count; ++i)
+				m_UserVariables.Add(m_Variables[i].Key, m_Variables[i].Value);
+			m_Variables = null;
+
+
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			if (m_UserVariables.Count > 0) {
+				m_TextComponent.text = L20n.Translate(m_Identifier, m_UserVariables);
+			} else {
+				m_TextComponent.text = L20n.Translate(m_Identifier);
+			}
+		}
+	}
+
+	[Serializable]
+	class L20nVariable
+	{
+		public string Key { get { return m_Key; } }
+		public UserVariable Value
+		{
+			get
+			{
+				if(m_Value.Variable != null)
+					return m_Value.Variable;
+				if(m_Value.Text != null && m_Value.Text != "")
+					return m_Value.Text;
+				return m_Value.Literal;
+			}
+		}
+
+		[SerializeField] private string m_Key;
+		[SerializeField] private L20nVariableValue m_Value;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		m_TextComponent.text = L20n.Translate(m_Identifier);
+	
+	[Serializable]
+	class L20nVariableValue
+	{
+		public int Literal = 0;
+		public string Text = "";
+		public L20nCore.External.UserHashValue Variable = null;
 	}
 }
