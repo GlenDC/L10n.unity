a671bcb7ecf65c9cc088265943f9fdb69c75cc7f
decauwsemaecker.glen@gmail.com <decauwsemaecker.glen@gmail.com>
2016-03-28 18:33:09 -0300

    add editor script to do a prober full export

---
+++ package/Assets/Editor/FullExporter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System;
+
+public class FullExport : MonoBehaviour
+{
+	[MenuItem("Tools/Full Export")]
+	private static void NewMenuOption ()
+	{
+		var fileName = "l20n.unitypackage";
+		var files = new String[] { "Assets/L20n", "Assets/Resources" };
+
+		AssetDatabase.ExportPackage (
+			files, fileName,
+			ExportPackageOptions.Interactive | ExportPackageOptions.Recurse | 
+			ExportPackageOptions.IncludeDependencies);
+	}
+}
