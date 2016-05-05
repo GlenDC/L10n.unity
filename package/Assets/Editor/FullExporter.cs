c668f76ea6593b87f02bcb80d86b233a9793696c
decauwsemaecker.glen@gmail.com <decauwsemaecker.glen@gmail.com>
2016-05-04 22:47:40 -0300

    fix L20n Plugin locations and fix some font thing of examples

---
+++ package/Assets/Editor/FullExporter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System;
+
+public class FullExport : MonoBehaviour
+{
+	const string version = "1.0";
+	[MenuItem("Tools/Full Export")]
+	private static void NewMenuOption ()
+	{
+		var fileName = String.Format("l20n_v{0}.unitypackage", version);
+		var files = new String[] { "Assets/L20n" };
+		
+		AssetDatabase.ExportPackage (
+			files, fileName,
+			ExportPackageOptions.Interactive | ExportPackageOptions.Recurse | 
+			ExportPackageOptions.IncludeDependencies);
+	}
+}
